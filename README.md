f903e731512a4a241f454efd9e59f20fddf820b1
keisuke3 <ishii@gizumo-inc.jp>
2021-11-22 00:59:28 +0900

    add: import component repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# bit-import-app
+
+bitでexportされたコンポーネントをimportするリポジトリ
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hello />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Hello from '@bit/keisuke3.bit-export-app.hello';
+
+export default Vue.extend({
+  components: {
+    Hello,
+  },
+});
+</script>
